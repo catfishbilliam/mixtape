1079aabfdaacf679cd8e495e9726934846f0b636
William Conway-Harris <williamconway-harris@mac.lan>
2025-06-04 23:28:57 -0400

    resolve heroku port error

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
